5ece8f572772666f61d99fda349a13f93c6a842d
Shiyang-Zhao <shiyang2575326696@gmail.com>
2023-08-30 16:08:06 -0700

    Update

--- src/components/User/js/UserList.js
+++ src/components/User/js/UserList.js
@@ -6,90 +6,112 @@ import ReactPaginate from "react-paginate";
 import axios from "axios";
 import { CSVLink } from "react-csv";
 import { API_URL } from "config";
-import useWebSocket from "react-use-websocket";
+import SockJS from 'sockjs-client';
+import { Stomp } from '@stomp/stompjs'
 
 export default function UserList({ state }) {
 
+    var sock = new SockJS("http://localhost:8080/EmploVerse/ws");
+    let stompClient = Stomp.over(sock);
+    sock.onopen = function () {
+        console.log('open');
+    }
+    stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', function (greeting) {
+            //you can execute any function here
+            console.log(greeting.body);
+        });
+    });
 
-    // const navigate = useNavigate();
-    // const [allUsersList, setAllUsersList] = useState([]);
+    const sendMessageToServer = () => {
+        stompClient.send('/app/hello', {}, "Hello, server!");
+    }
 
-    // useEffect(() => {
-    //     getAllUsersList();
-    // }, []);
 
-    // const getAllUsersList = async () => {
-    //     try {
-    //         const response = await axios.get(`${API_URL}/users/`, {
-    //             headers: {
-    //                 Authorization: state.cookies.jwt,
-    //             },
-    //         });
-    //         setAllUsersList(response.data);
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // };
-    // const handleUserClick = (userId) => {
-    //     // Handle navigation to user details page when a user is clicked
-    //     navigate(`/user/${userId}`);
-    // };
+    const navigate = useNavigate();
+    const [allUsersList, setAllUsersList] = useState([]);
 
-    // return (
-    //     <div className={styles.userListContainer}>
-    //         {/* Add a search bar if necessary */}
-    //         <div className={styles.searchBar}>
-    //             <i className={`fa-solid fa-magnifying-glass fa-xl ${styles.searchIcon}`}></i>
-    //             <input type="text" placeholder="Search users..." />
-    //         </div>
+    useEffect(() => {
+        getAllUsersList();
+    }, []);
 
-    //         {/* Render the user list */}
-    //         <div className={styles.userList}>
-    //             {allUsersList.map((user) => (
-    //                 <div
-    //                     key={user.id}
-    //                     className={styles.userCard}
-    //                     onClick={() => handleUserClick(user.id)}
-    //                 >
-    //                     <div className={styles.userAvatar}>
-    //                         {/* Display user avatar or profile picture */}
-    //                         <img src={user.avatar} alt={`Avatar of ${user.name}`} />
-    //                     </div>
-    //                     <div className={styles.userInfo}>
-    //                         <h3>{user.name}</h3>
-    //                         <p>Email: {user.email}</p>
-    //                         {/* Add any other user information you want to display */}
-    //                     </div>
-    //                 </div>
-    //             ))}
-    //         </div>
+    const getAllUsersList = async () => {
+        try {
+            const response = await axios.get(`${API_URL}/users/`, {
+                headers: {
+                    Authorization: state.cookies.jwt,
+                },
+            });
+            setAllUsersList(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    const handleUserClick = (userId) => {
+        // Handle navigation to user details page when a user is clicked
+        navigate(`/user/${userId}`);
+    };
 
-    //         {/* Add pagination if necessary */}
-    //         <ReactPaginate
-    //             previousLabel={"Previous"}
-    //             nextLabel={"Next"}
-    //             breakLabel={"..."}
-    //             pageCount={10} // Example: Replace with the actual number of pages
-    //             marginPagesDisplayed={2}
-    //             pageRangeDisplayed={5}
-    //             onPageChange={(selectedPage) => {
-    //                 // Handle pagination change here
-    //             }}
-    //             containerClassName={styles.paginationContainer}
-    //             activeClassName={styles.paginationActive}
-    //         />
+    return (
 
-    //         {/* Add a CSV export button if necessary */}
-    //         <CSVLink
-    //             data={allUsersList}
-    //             filename={"user_list.csv"}
-    //             className={styles.exportButton}
-    //         >
-    //             Export User List to CSV
-    //         </CSVLink>
+        <div className={styles.userListContainer}>
+            <div>
+                <button onClick={sendMessageToServer} className={styles.ws}>Send Message to Server</button>
+            </div>
+            {/* Add a search bar if necessary */}
+            <div className={styles.searchBar}>
+                <i className={`fa-solid fa-magnifying-glass fa-xl ${styles.searchIcon}`}></i>
+                <input type="text" placeholder="Search users..." />
+            </div>
 
-    //         {/* Add the footer component */}
-    //         <Footer />
-    //     </div>
-    // );
+            {/* Render the user list */}
+            <div className={styles.userList}>
+                {allUsersList.map((user) => (
+                    <div
+                        key={user.id}
+                        className={styles.userCard}
+                        onClick={() => handleUserClick(user.id)}
+                    >
+                        <div className={styles.userAvatar}>
+                            {/* Display user avatar or profile picture */}
+                            <img src={user.avatar} alt={`Avatar of ${user.name}`} />
+                        </div>
+                        <div className={styles.userInfo}>
+                            <h3>{user.name}</h3>
+                            <p>Email: {user.email}</p>
+                            {/* Add any other user information you want to display */}
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            {/* Add pagination if necessary */}
+            <ReactPaginate
+                previousLabel={"Previous"}
+                nextLabel={"Next"}
+                breakLabel={"..."}
+                pageCount={10} // Example: Replace with the actual number of pages
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={(selectedPage) => {
+                    // Handle pagination change here
+                }}
+                containerClassName={styles.paginationContainer}
+                activeClassName={styles.paginationActive}
+            />
+
+            {/* Add a CSV export button if necessary */}
+            <CSVLink
+                data={allUsersList}
+                filename={"user_list.csv"}
+                className={styles.exportButton}
+            >
+                Export User List to CSV
+            </CSVLink>
+
+            {/* Add the footer component */}
+            <Footer />
+        </div>
+    );
 }
